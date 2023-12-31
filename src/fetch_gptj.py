0709cebc2fdc6f9b0fdb7b8bdcfacb2df83aa1b0
Dipendra Misra <dipendrakumarmisra@gmail.com>
2023-12-30 20:09:16 -0500

    Update fetch_gptj.py

--- src/fetch_gptj.py
+++ src/fetch_gptj.py
@@ -2,7 +2,7 @@ import torch
 from transformers import GPTJForCausalLM
 
 # The purpose of this file is to pre-download GPTJ model so that parallel experiments can be run
-# without
+# without each experiment fetching the GPTJ model which leads to failures due to locking
 
 # Load model
 model = GPTJForCausalLM.from_pretrained(
